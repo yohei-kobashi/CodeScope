4236b10b89fb1fff6d86ad671567550604c2d798
yohei-kobashi <yohei.kobashi@gmail.com>
2026-01-21 13:58:05 +0900

    misc

--- code_translation/run_eval.sh
+++ code_translation/run_eval.sh
@@ -8,10 +8,10 @@ cd CodeScope
 source env_c/bin/activate
 cd code_translation
 # Qwen3-Coder 30B base model
-python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Qwen3.jsonl --output_path result/eval_Qwen3.jsonl --request_retries 5 --max_workers 128
+python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Qwen2.5_Coder_7B_dapo_0116.jsonl --output_path result/eval_Qwen2.5_Coder_7B_dapo_0116.jsonl --request_retries 5 --max_workers 128
 
 # # Seed-Coder-8B base model
-python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Seed-Coder.jsonl --output_path result/eval_Seed-Coder.jsonl --request_retries 5 --max_workers 128
+python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Qwen2.5_Coder_7B_drgrpo_0116.jsonl --output_path result/eval_Qwen2.5_Coder_7B_drgrpo_0116.jsonl --request_retries 5 --max_workers 128
 
 # Qwen2.5-Coder-7B-Instruct base model
-python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Qwen2.5.jsonl --output_path result/eval_Qwen2.5.jsonl --request_retries 5 --max_workers 128
+python evaluator/run_multiple.py --jsonl_path result/code_translation_eval_Qwen2.5_Coder_7B_grpo_0116.jsonl --output_path result/eval_Qwen2.5_Coder_7B_grpo_0116.jsonl --request_retries 5 --max_workers 128
